1e419e2d4dde64c3eb408aaf0ad1fb17ce6a58e8
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2020-12-21 12:14:12 +0200

    Update uploader.js

--- generators/app/uploader.js
+++ generators/app/uploader.js
@@ -1,9 +1,56 @@
-// const pageObjects = require("puppeteer-page-objects");
+"use strict";
+
 const puppeteer = require("puppeteer");
+const LoginPage = require('puppeteer-page-objects/common/pageObjects/Login/LoginPage');
+const DevSpacesPage = require('puppeteer-page-objects/common/pageObjects/DevSpaces/DevSpacesPage');
+const TheiaPage = require('puppeteer-page-objects/common/pageObjects/Theia/TheiaPage');
 
 exports.execute = async data => {
-    const browser = await puppeteer.launch({ headless: data.headless });
-    const page = await browser.newPage();
+    const webIdeUrl = data.url;
+    const dsName = data.spaceName;
+    const dsTypeName = data.spaceType;
+    const username = "puppeteermanualtest01@gmail.com";
+    const password = "Mars_1209";
 
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 30,
+        defaultViewport: null,
+        args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--start-maximized", "--ignore-certificate-errors", "--window-size=1920,1080"]
+    });
+    const page = await new LoginPage().loginAndNavigate(browser, webIdeUrl, username, password);
+
+    await page.goto(webIdeUrl);
+    const devSpacesPage = new DevSpacesPage();
+    await devSpacesPage.deleteDevSpaceIfMaxRunning(page);
+    await devSpacesPage.navigateToCreateNewDevSpace(page)
+    await devSpacesPage.insertNewDevSpaceName(page, dsName);
+    await devSpacesPage.selectDevSpaceType(page, dsTypeName);
+    await devSpacesPage.createNewDevSpace(page);
+    await devSpacesPage.waitForDevSpaceRunningStatus(page, dsName);
+    await devSpacesPage.enterDevSpace(page, dsName);
+    const theiaPage = new TheiaPage();
+    await theiaPage.waitforTheiaToLoad(page);
+    await theiaPage.openWorkSpaceFolderDialog(page);
+    await theiaPage.openWorkspace(page, "user");
+    await theiaPage.createFolder(page, "default-plugins");
+    await theiaPage.selectNode(page, "default-plugins", "", true, "/home/user");
+
+    const [fileChooser] = await Promise.all([
+        page.waitForFileChooser(),
+        theiaPage.goThroughMenubarItemsAndSelect(page, ["File", "Upload Files..."])
+    ]);
+
+    await fileChooser.accept([data.vsixPath]);
+    await theiaPage.checkNotificationText(page, "Uploading Files...: 0 out of 1");
+    await page.waitFor(10000);
     await page.goto(data.url);
+    await devSpacesPage.stopDevSpace(page, dsName);
+    await devSpacesPage.waitForDevSpaceStoppedStatus(page, dsName, 1000000);
+    await devSpacesPage.startDevSpace(page, dsName);
+    await devSpacesPage.waitForDevSpaceStartingStatus(page, dsName, 1000000);
+    await devSpacesPage.waitForDevSpaceRunningStatus(page, dsName, 1000000);
+
+    await devSpacesPage.enterDevSpace(page, dsName);
+    await theiaPage.waitForTheiaToBeVisible(page);
 }
